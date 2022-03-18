51af40b945414d2c9171ce5a95df56013ffa02d4
pwhb <pwhbdev@gmail.com>
2022-03-18 10:46:13 +0630

    switched to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,31 @@
-// import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
 
-import vercel from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: vercel()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
+// import adapter from '@sveltejs/adapter-auto';
 
-export default config;
+// // import vercel from '@sveltejs/adapter-vercel';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
